44308a12838d79ab88cd592515878cf5aef347cc
yuta.name <yuta_matsuo@r.recruit.co.jp>
2022-01-20 23:32:26 +0900

    index を作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -166,4 +166,15 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+
+    public void destroy() throws ServletException, IOException {
+        if (checkToken()) {
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+        }
+
+    }
+
 }
