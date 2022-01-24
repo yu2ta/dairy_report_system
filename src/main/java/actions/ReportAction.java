a0a45b1c5a61f51d5f09631ca2426b94fa119518
yuta.name <yuta_matsuo@r.recruit.co.jp>
2022-01-24 19:22:44 +0900

    createの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.view.EmployeeView;
 import actions.view.ReportView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.ReportService;
 
 public class ReportAction extends ActionBase {
@@ -60,5 +62,47 @@ public class ReportAction extends ActionBase {
 
         //新規登録画面を表示
         forward(ForwardConst.FW_REP_NEW);
+    }
+
+    public void create() throws ServletException, IOException {
+        if (checkToken()) {
+
+            LocalDate day = null;
+            if (getRequestParam(AttributeConst.REP_DATE) == null) {
+                day = LocalDate.now();
+            } else {
+                day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+            }
+
+            //セッションスコープからログイン中の従業員情報を取得
+            EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+            //パラメータの値から日報情報を作成
+            ReportView rv = new ReportView(
+                    null,
+                    ev,
+                    day,
+                    getRequestParam(AttributeConst.REP_TITLE),
+                    getRequestParam(AttributeConst.REP_CONTENT),
+                    null, null);
+
+            List<String> errors = service.create(rv);
+
+            //登録中にエラーがあった場合
+            if (errors.size() > 0) {
+                putRequestScope(AttributeConst.TOKEN, getTokenId());
+                putRequestScope(AttributeConst.REPORT, ev);
+                putRequestScope(AttributeConst.ERR, errors);
+
+                forward(ForwardConst.FW_REP_NEW);
+
+            } else {
+                //セッションに登録完了のメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+                //リダイレクト
+                redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
+            }
+
+        }
     }
 }
